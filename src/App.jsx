1c66d67d26b873b9c49260dbf1ea179e1ca9543f
Ishan Kumar <ishankumar@Ishans-MacBook-Air.local>
2024-06-22 19:25:03 +0530

    Using Recoil to Re-render app , boost performance in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
-import React, { useContext, useState } from "react";
-import { CountContext } from "../context";
+import React from "react";
 import './App.css'; // Importing the CSS file
 import './index.css'
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./store/atoms/count";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="app-container">
       <div className="top-bar">Counter App</div>
-      <CountContext.Provider value={count}>
-        <Count setCount={setCount} />
-      </CountContext.Provider>
+        <RecoilRoot>
+          <Count  />
+        </RecoilRoot>
       <div className="bottom-bar"> Count</div>
     </div>
   );
 }
 
-function Count({ setCount }) {
+function Count() {
   return (
     <div>
       <CountRenderer />
-      <Buttons setCount={setCount} />
+      <Buttons />
     </div>
   );
 }
 
 function CountRenderer() {
-  const count = useContext(CountContext);
+  const count = useRecoilValue(countAtom);
 
   return <div className="counter">{count}</div>;
 }
 
-function Buttons({ setCount }) {
-  const count = useContext(CountContext);
+function Buttons() {
+  const [count,setCount ]= useRecoilState(countAtom);
   return (
     <div className="buttons-container">
       <button onClick={() => setCount(count + 1)}>Increase</button>
